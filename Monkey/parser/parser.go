0feb26745b151950cec693da01ca0678fbfa3aed
Xiao tian Liu <v-xiaotiliu@microsoft.com>
2025-09-11 10:18:05 +0800

    [Add] returnstatement

--- Monkey/parser/parser.go
+++ Monkey/parser/parser.go
@@ -54,6 +54,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -71,6 +73,19 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	if !p.expectPeek(token.ASSIGN) {
 		return nil
 	}
+
+	// TODO: We're skipping the expressions until we reach a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
 
 	// TODO: We're skipping the expressions until we reach a semicolon
 	for !p.curTokenIs(token.SEMICOLON) {
